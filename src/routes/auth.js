88db389d44bbc51b7e53abc228550e4ed69f94a5
Barys Liutkouski <boris.liutkovski@gmail.com>
2019-02-25 22:27:23 +0300

    upd

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
-const { jwtSecret } = require('../config')
+const { jwtSecret } = require('../config/index')
 const bodyParser = require('body-parser')
 
 const authRouter = router
--- test/news.js
+++ test/news.js
@@ -1,8 +1,6 @@
-process.env.NODE_ENV = 'test'
-
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const server = require('../src/index')
+const server = require('../src/index/test')
 const News = require('../src/models/news')
 chai.should()
 
